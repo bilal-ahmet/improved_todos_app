932a9e95bb6bcd788d75504c07027fb082fb23c0
bilal-ahmet <akpinarbilalahmet@gmail.com>
2024-02-23 15:13:24 +0300

    edit_with_stateful_consumer_wiget

--- lib/widgets/todo_list_item_widget.dart
+++ lib/widgets/todo_list_item_widget.dart
@@ -3,20 +3,70 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todos_app_riverpod/models/todo_model.dart';
 import 'package:todos_app_riverpod/provider/all_providers.dart';
 
-class TodoListItemWidget extends ConsumerWidget {
+class TodoListItemWidget extends ConsumerStatefulWidget {
+
   TodoModel item;
   TodoListItemWidget({required this.item, super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return ListTile(
-      leading: Checkbox(
-        value: item.completed,
-        onChanged: (value) {
-          ref.read(todoListProvider.notifier).toggle(item.id);
+  ConsumerState<ConsumerStatefulWidget> createState() => _TodoListItemWidgetState();
+}
+
+class _TodoListItemWidgetState extends ConsumerState<TodoListItemWidget> {
+
+  late FocusNode _textFocusNode;
+  late TextEditingController _textController;
+
+  bool _hasFocus = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _textFocusNode = FocusNode();
+    _textController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _textFocusNode.dispose();
+    _textController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Focus(
+      onFocusChange: (isFocused) {
+        if(!isFocused){
+          setState(() {
+            _hasFocus = false;
+          });
+
+          ref.read(todoListProvider.notifier).edit(id: widget.item.id, newDescription: _textController.text);
+        }
+
+      },
+      child: ListTile(
+        onTap: () {
+          setState(() {
+            _hasFocus = true;
+            _textFocusNode.requestFocus();
+            _textController.text = widget.item.description;
+          });
+
+          
         },
+        leading: Checkbox(
+          value: widget.item.completed,
+          onChanged: (value) {
+            ref.read(todoListProvider.notifier).toggle(widget.item.id);
+          },
+        ),
+        title: _hasFocus ? TextField(controller: _textController, focusNode: _textFocusNode,) : Text(widget.item.description),
       ),
-      title: Text(item.description),
     );
   }
 }
+
